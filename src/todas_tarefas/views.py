a1cce197eff1d4e75c0af18a1ed1a6fc0d5c2407
cettdev3 <desenvolvedor3@cett.org.br>
2024-02-15 13:38:33 -0300

    Adicionado Filtros em Todas os Jobs

--- src/todas_tarefas/views.py
+++ src/todas_tarefas/views.py
@@ -14,24 +14,27 @@ def Todas_Tarefas(request):
 @login_required(login_url='/')
 def Get_Users_Designante(request):
     solicitacao_id = request.GET.get('solicitacao_id')
-    usuarios_list = []
-    users_apend = []
-    usuarios = Demandas.objects.filter(peca__solicitacao=solicitacao_id).all()
-    for usuario in usuarios:
-        if usuario.designante_id not in users_apend:
-            users_apend.append(usuario.designante_id)
-            usuarios_list.append(usuario)
+    if solicitacao_id:
+        usuarios_list = []
+        users_apend = []
+        usuarios = Demandas.objects.filter(peca__solicitacao=solicitacao_id).all()
+        for usuario in usuarios:
+            if usuario.designante_id not in users_apend:
+                users_apend.append(usuario.designante_id)
+                usuarios_list.append(usuario)
+    else:
+        usuarios_list = []
     return render(request, 'ajax/ajax_designante.html', {'usuarios': usuarios_list})
 
 @login_required(login_url='/')
 def Get_Peca_Filter(request):
     solicitacao_id = request.GET.get('solicitacao_id')
-    designante_id = request.GET.get('designante_id')
-    usuarios_list = []
-    users_apend = []
-    usuarios = Demandas.objects.filter(peca__solicitacao=solicitacao_id).all()
-    for usuario in usuarios:
-        if usuario.designante_id not in users_apend:
-            users_apend.append(usuario.designante_id)
-            usuarios_list.append(usuario)
-    return render(request, 'ajax/ajax_designante.html', {'usuarios': usuarios_list})
+    designante_id = request.GET.get('designante')
+    if solicitacao_id:
+        if designante_id:
+            demandas = Demandas.objects.filter(peca__solicitacao=solicitacao_id,designante_id=designante_id).all()
+        else:
+            demandas = Demandas.objects.filter(peca__solicitacao=solicitacao_id).all()
+    else:
+        demandas = Demandas.objects.all()
+    return render(request, 'ajax/ajax_tbl_demandas.html', {'demandas': demandas})
